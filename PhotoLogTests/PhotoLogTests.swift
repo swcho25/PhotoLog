b76ca236e3a19e66d721bb1a2d63b28cd565d05f
seokwon cho <dkf1141@naver.com>
2025-06-05 21:34:55 +0900

    initial commit

---
+++ PhotoLogTests/PhotoLogTests.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoLogTests.swift
+//  PhotoLogTests
+//
+//  Created by seokwon on 6/5/25.
+//
+
+import Testing
+@testable import PhotoLog
+
+struct PhotoLogTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
